83c4bfa0f43482c5c44d99b7b584cbb459c8377b
srinath39 <ashwinisrinath2580@gmail.com>
2026-04-16 21:15:18 +0530

    Avoid the Hardcoded Sql Insert query

--- src/main/java/com/springBoot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLinerRunner.java
+++ src/main/java/com/springBoot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLinerRunner.java
@@ -1,5 +1,6 @@
 package com.springBoot.learn_jpa_and_hibernate.course.jdbc;
 
+import com.springBoot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,6 @@ public class CourseJdbcCommandLinerRunner implements CommandLineRunner {
     // command line runner , By default spring runs this method while application start up
     @Override
     public void run(String... args) throws Exception {
-        repository.insertRecord();
+        repository.insertRecord(new Course(2,"System Design","Srinath"));
     }
 }
--- src/main/java/com/springBoot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/springBoot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.springBoot.learn_jpa_and_hibernate.course.jdbc;
 
+import com.springBoot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,10 +13,10 @@ public class CourseJdbcRepository {
 
     private String INSERT_QUERY= """
                 insert into course (id, name,author) 
-                values (1,'LLD','Srinath');
+                values (?,?,?);
                 """;
 
-    public void insertRecord(){
-        springJdbcTemplate.update(INSERT_QUERY);
+    public void insertRecord(Course course){
+        springJdbcTemplate.update(INSERT_QUERY,course.getId(),course.getName(),course.getAuthor());
     }
 }
