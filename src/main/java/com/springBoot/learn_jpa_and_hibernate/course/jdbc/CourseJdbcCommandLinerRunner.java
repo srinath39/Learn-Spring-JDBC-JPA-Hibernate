49e6b3cbddf2fda9d2b45632863f6714822dc081
srinath39 <ashwinisrinath2580@gmail.com>
2026-04-16 21:04:27 +0530

    All about spring jdbc

---
+++ src/main/java/com/springBoot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLinerRunner.java
@@ -0,0 +1,19 @@
+package com.springBoot.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CourseJdbcCommandLinerRunner implements CommandLineRunner {
+
+    @Autowired
+    private CourseJdbcRepository repository;
+
+    // command line runner , By default spring runs this method while application start up
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insertRecord();
+    }
+}
